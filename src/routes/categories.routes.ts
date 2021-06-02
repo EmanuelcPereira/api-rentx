124ebe090f543bed7c09648d123411f5cd153bee
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-06-02 13:47:01 -0300

    feat: add create category service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,9 +9,15 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  categoriesRepository.create({ name, description });
+  ;
 
   return res.status(201).send();
+});
+
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoriesRepository.list();
+
+  return res.status(201).json(all);
 });
 
 export { categoriesRoutes };
