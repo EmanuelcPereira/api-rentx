c97175ce95ad4cb6a64892006d194f808a998652
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-06-03 15:17:32 -0300

    feat: add import category use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/UseCases/CreateCategory";
+import { importCategoryController } from "../modules/cars/UseCases/ImportCategory";
 import { listCategoriesController } from "../modules/cars/UseCases/ListCategory";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) => {
   return createCategoryController.handle(req, res);
@@ -11,6 +16,10 @@ categoriesRoutes.post("/", (req, res) => {
 
 categoriesRoutes.get("/", (req, res) => {
   return listCategoriesController.handle(req, res);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  return importCategoryController.handle(req, res);
 });
 
 export { categoriesRoutes };
