202dc74d209397351763e19cea648156a8588ff6
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-06-04 10:50:43 -0300

    refactor: refactor categories repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import { createCategoryController } from "../modules/cars/UseCases/CreateCategory";
+import createCategoryController from "../modules/cars/UseCases/CreateCategory";
 import { importCategoryController } from "../modules/cars/UseCases/ImportCategory";
 import { listCategoriesController } from "../modules/cars/UseCases/ListCategory";
 
@@ -11,7 +11,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post("/", (req, res) => {
-  return createCategoryController.handle(req, res);
+  return createCategoryController().handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
