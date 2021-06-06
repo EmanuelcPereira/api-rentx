20f7950469a4d95e34672a3cf37ee2ce61d9d18b
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-06-06 16:20:08 -0300

    chore: add express async errors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 import cors from "cors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 import "./database";
 import "./shared/container";
+
 
 const app = express();
 const port = 3333;
@@ -17,6 +19,17 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
