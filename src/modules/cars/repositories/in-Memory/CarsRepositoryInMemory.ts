ee8667b0d583704a395c7ac2d04ce66535af0195
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-06-09 16:43:03 -0300

    feat: add find available function

--- src/modules/cars/repositories/in-Memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-Memory/CarsRepositoryInMemory.ts
@@ -43,10 +43,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
   ): Promise<Car[]> {
     const availableCars = this.cars.filter((car) => {
       if (
-        car.available === true &&
-        ((brand && car.brand === brand) ||
-          (category_id && car.category_id === category_id) ||
-          (name && car.name === name))
+        car.available === true ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
       ) {
         return car;
       }
@@ -54,6 +54,10 @@ class CarsRepositoryInMemory implements ICarsRepository {
     });
 
     return availableCars;
+  }
+
+  async findById(id: string): Promise<Car> {
+    return this.cars.find((car) => car.id === id);
   }
 }
 
