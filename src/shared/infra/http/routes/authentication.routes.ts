b932067251d6c8ea756259f912ff01e32fee26e0
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-06-14 17:49:06 -0300

    feat: add refresh token route

--- src/shared/infra/http/routes/authentication.routes.ts
+++ src/shared/infra/http/routes/authentication.routes.ts
@@ -1,10 +1,13 @@
 import { AuthenticateUserController } from "@modules/accounts/useCases/AuthenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/RefreshToken/RefreshTokenController";
 import { Router } from "express";
 
 const authenticationsRoutes = Router();
 
 const authenticationsUserController = new AuthenticateUserController();
+const refreshTokenController = new RefreshTokenController();
 
 authenticationsRoutes.post("/sessions", authenticationsUserController.handle);
+authenticationsRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticationsRoutes };
