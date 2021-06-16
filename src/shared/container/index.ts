df2645a41cf3db393be89c131815f566a0c2d5a1
Emanuel Pereira <emanuelcdpr@gmail.com>
2021-06-15 21:26:21 -0300

    refactor: change injections to providers

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,4 +1,6 @@
-import "@shared/container/providers";
+import "@shared/container/providers/DateProvider";
+import "@shared/container/providers/MailProvider";
+import "@shared/container/providers/StorageProvider";
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
